fd2b05324d573258e0ff406d8110db8e4391204e
2076462733 <49665594+2076462733@users.noreply.github.com>
2022-05-13 09:38:03 +0800

    头部国旗修改

--- src/view/layout/header/Topbar.vue
+++ src/view/layout/header/Topbar.vue
@@ -172,7 +172,7 @@ export default {
   name: "KTTopbar",
   data() {
     return {
-      languageFlag: process.env.BASE_URL + "media/svg/flags/226-united-states.svg",
+      languageFlag: process.env.BASE_URL + "media/svg/flags/034-china.svg",
       languages: i18nService.languages
     };
   },
