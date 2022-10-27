a50957abf422d8de6c1b6ab24970998478806f6c
chaoxiaoshu-mx <leukotrichia@163.com>
2022-10-27 14:19:52 +0800

    解决多次登录和修改logo的bug

--- src/core/services/store/config.module.js
+++ src/core/services/store/config.module.js
@@ -18,7 +18,7 @@ const local_url =
 export default {
   state: {
     config: config,
-    initial: config
+    initial: config,
   },
   getters: {
     /**
@@ -70,7 +70,6 @@ export default {
       state.config = payload;
     },
     [RESET_LAYOUT_CONFIG](state) {
-      console.log("RESET_LAYOUT_CONFIG", state.initial)
       ApiService.post(local_url + "/system/logo/index")
       .then(({ data }) => {
         if (data.code == 200) {
