165c858c405536d24798b106aa3b13929d608979
chaoxiaoshu-mx <leukotrichia@163.com>
2023-03-08 11:12:19 +0800

    优化设备监控

--- src/view/pages/device-watch/device-detail/index.vue
+++ src/view/pages/device-watch/device-detail/index.vue
@@ -27,6 +27,7 @@ import {device_group_drop} from "@/api/asset";
 import PluginAPI from "@/api/plugin.js"
 import {getDeviceTree} from "@/api/device";
 import PluginCharts from "./PluginCharts.vue"
+import useTree from "./useTree";
 
 export default defineComponent({
   name: "DeviceDetail",
@@ -51,91 +52,18 @@ export default defineComponent({
     watch(filterValue, value => {
       pluginTree.value.filter(value)
     })
+
     function filterNode(value, data) {
       if (!value) return true;
       return data[defaultProps.label].indexOf(value) !== -1;
     }
 
-    /**
-     * 1. 初始化时在树组件中显示业务下的所有设备分组
-     * 2. 点击设备分组后加载该分组下的所有设备
-     */
-    let pluginData = ref([])
-    let groupId = "";
 
-    function loadNode(node, resolve) {
-      // 默认加载一级节点
-      if (node.level == 0) {
-        device_group_drop({ business_id })
-          .then(({ data }) => {
-            let arr = data.data.map(item => { return { label: item.device_group, id: item.id } })
-            return resolve(arr);
-          })
-      }
+    let { 
+      loadNode,
+      pluginOptions, pluginTsl, device, nodeClick
+    } = useTree(business_id);
 
-      // 点击了一级分类，加载二级节点
-      if (node.level == 1) {
-        groupId = node.data.id;
-        let data ={current_page: 1, per_page: 9999, asset_id: groupId}
-        getDeviceTree(data)
-          .then(({data}) => {
-            if (data.code == 200) {
-              if (!data.data.data) {
-                return resolve([])
-              }
-              let arr = data.data.data.map(item => {
-                item.label = item.device_name;
-                if (!item.children) {
-                  item.leaf = true;
-                }
-                return item;
-              })
-              return resolve(arr);
-            }
-          })
-      }
-
-      if (node.level == 2) {
-        if (node.data.children) {
-          let arr = node.data.children.map(item => {
-            item.id = item.device;
-            item.label = item.device_name;
-            item.leaf = true;
-            return item;
-          })
-          return resolve(arr);
-        }
-      }
-
-    }
-
-    let pluginOptions = ref([]);
-    let pluginTsl = ref([]);
-    let canvasStyle = ref({});
-    let device = ref({})
-
-    /**
-     * 点击设备显示插件图表
-     * @param node
-     */
-    function nodeClick(node) {
-      device.value = node;
-      if (node.leaf && node.device && node.type) {
-        let param = {"current_page": 1, "per_page": 10, "id": node.type}
-          PluginAPI.page(param)
-              .then(({data}) => {
-                if (data.code == 200 && data.data && data.data.data && data.data.data.length > 0) {
-                  let plugin = JSON.parse(data.data.data[0].chart_data);
-                  pluginOptions.value = JSON.parse(JSON.stringify(plugin.chart));
-                  pluginTsl.value = JSON.parse(JSON.stringify(plugin.tsl.properties));
-                } else {
-                  pluginOptions.value = [];
-                }
-              })
-      } else {
-        pluginOptions.value = [];
-      }
-    }
 
     function back() {
       router.push({ name: "DeviceWatch" })
@@ -147,11 +75,9 @@ export default defineComponent({
       defaultProps,
       filterValue,
       pluginTree,
-      pluginData,
       filterNode,
       loadNode,
       nodeClick,
-      canvasStyle,
       device,
       pluginOptions,
       pluginTsl
