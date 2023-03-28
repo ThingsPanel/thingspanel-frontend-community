ae5363d48aa93df7f365e04ec7e3e427c858c734
chaoxiaoshu-mx <leukotrichia@163.com>
2023-03-28 19:37:07 +0800

    历史曲线增加时间选择

--- src/view/pages/device-watch/device-detail/components/Curve.vue
+++ src/view/pages/device-watch/device-detail/components/Curve.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="chart-div" :style="getChartStyle()">
-    <div  v-if="showHeader" class="chart-header">
+    <div v-if="showHeader" class="chart-header">
 
       <span class="title">{{ optionData.name }}</span>
       <div class="tool-right">
 
         <!-- 采样区间 -->
-        <el-button class="tool-item" size="mini" icon="el-icon-date" @click="handleClickRange"></el-button>
+        <el-button class="tool-item" size="mini" icon="el-icon-date" @click="handleShowRange"></el-button>
 
         <!-- 采样周期  -->
         <el-dropdown @command="handlePeriodCommand">
@@ -36,7 +36,7 @@
             <el-dropdown-item command="1800">半小时</el-dropdown-item>
           </el-dropdown-menu>
         </el-dropdown>
-<!--        <el-button v-if="optionData.controlType == 'history'" class="tool-item" size="mini" icon="el-icon-picture-outline"></el-button>-->
+        <!--        <el-button v-if="optionData.controlType == 'history'" class="tool-item" size="mini" icon="el-icon-picture-outline"></el-button>-->
 
         <el-button class="tool-item" size="mini" icon="el-icon-more" @click="showConfiguration"></el-button>
       </div>
@@ -45,8 +45,7 @@
 
     <div style="width: 100%; height:calc(100% - 60px);position: absolute;top:40px;bottom:20px" ref="chart"></div>
 
-    <el-dialog class="el-dark-dialog" title="配置" width="30%" :append-to-body="true"
-               :visible.sync="configurationVisible">
+    <el-dialog class="el-dark-dialog" title="配置" width="30%" :append-to-body="true" :visible.sync="configurationVisible">
       <span></span>
       <span slot="footer" class="dialog-footer">
         <el-button @click="configurationVisible = false">取 消</el-button>
@@ -54,37 +53,33 @@
       </span>
     </el-dialog>
 
-    <el-dialog title="采样区间" width="30%" :append-to-body="true"
-               :visible.sync="rangeDialogVisible">
-      <span>
-        <el-date-picker
-          v-model="value2"
-          type="datetime"
-          placeholder="选择日期时间"
-          align="right"
-          :picker-options="pickerOptions">
-        </el-date-picker>
-      </span>
-      <span>
-        <el-date-picker
-          v-model="value2"
-          type="datetime"
-          placeholder="选择日期时间"
-          align="right"
-          :picker-options="pickerOptions">
-        </el-date-picker>
-      </span>
+    <el-dialog class="range-dialog" title="采样区间" width="500px" :append-to-body="true" :visible.sync="rangeDialogVisible">
+      <el-form>
+        <el-row>
+          <el-col :span="12">
+            <el-date-picker v-model="range.startTime" type="datetime" placeholder="选择起始时间" align="right"
+              :picker-options="pickerOptions">
+            </el-date-picker>
+          </el-col>
+
+          <el-col :span="12">
+            <el-date-picker v-model="range.endTime" type="datetime" placeholder="选择结束时间" align="right"
+              :picker-options="pickerOptions">
+            </el-date-picker>
+          </el-col>
+        </el-row>
+      </el-form>
 
       <span slot="footer" class="dialog-footer">
         <el-button @click="rangeDialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="rangeDialogVisible = false">确 定</el-button>
+        <el-button type="primary" @click="handleRangeSubmit">确 定</el-button>
       </span>
     </el-dialog>
   </div>
 </template>
 
 <script>
-import {  historyValue } from "@/api/device";
+import { historyValue } from "@/api/device";
 
 // let Echarts = require('echarts/lib/echarts');
 
@@ -97,7 +92,7 @@ export default {
     },
     option: {
       type: [Object],
-      default: () => {return {}}
+      default: () => { return {} }
     },
     value: {
       type: [Object, String, Array],
@@ -105,7 +100,7 @@ export default {
     },
     device: {
       type: [Object],
-      default: () => {return {}}
+      default: () => { return {} }
     }
   },
   data() {
@@ -117,11 +112,52 @@ export default {
       params: {
         period: 300,   // 采样周期，默认最近5分钟
         rate: 10     // 采样频率，默认10秒
+      },
+      pickerOptions: {
+        disabledDate(time) {
+          return time.getTime() > Date.now();
+        },
+        shortcuts: [
+          {
+            text: '今天',
+            onClick(picker) {
+              picker.$emit('pick', new Date());
+            }
+          }, 
+          {
+            text: '昨天',
+            onClick(picker) {
+              const date = new Date();
+              date.setTime(date.getTime() - 3600 * 1000 * 24);
+              picker.$emit('pick', date);
+            }
+          }, 
+          {
+            text: '一周前',
+            onClick(picker) {
+              const date = new Date();
+              date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
+              picker.$emit('pick', date);
+            }
+          },
+          {
+            text: '一月前',
+            onClick(picker) {
+              const date = new Date();
+              date.setTime(date.getTime() - 3600 * 1000 * 24 * 30);
+              picker.$emit('pick', date);
+            }
+          }
+        ]
+      },
+      range: {
+        startTime: '',
+        endTime: '',
       }
     }
   },
   mounted() {
-    window.addEventListener("resize",() => {
+    window.addEventListener("resize", () => {
       this.myEcharts.resize();
     });
     this.optionData = JSON.parse(JSON.stringify(this.option));
@@ -154,40 +190,57 @@ export default {
     getHistory(mapping) {
       let attrs = mapping.map(item => item.name ? item.name : item);
       if (!attrs || attrs.length == 0) return;
-      let now = (new Date()).getTime();
-      let lastTime = now - (Number(this.params.period) * 1000);
+
+      let endTime = (new Date()).getTime();
+      let startTime = endTime - (Number(this.params.period) * 1000);
+
+      // 如果有选择时间区间，则以选择的时间区间为准
+      if (this.range.startTime && this.range.endTime) {
+        startTime = new Date(this.range.startTime).getTime();
+        endTime = new Date(this.range.endTime).getTime();
+      }
+      
       // let startTime = endTime-246060*1000;
       let rate = this.params.rate * 1000 * 1000;  // 微秒
       let attribute = attrs.concat(["systime"])
       let params = {
         device_id: this.device.device,
         attribute,
-        "start_ts": lastTime,
-        "end_ts": now,
+        "start_ts": startTime,
+        "end_ts": endTime,
         rate: rate + ""
       }
       historyValue(params)
-          .then(({data}) => {
-            if (data.code == 200) {
-              let series = [];
-              let sysTimes = [];
-              if (data.data && JSON.stringify(data.data) != "{}") {
-                for (let i = 0; i < attrs.length; i++) {
-                  series.push({ data: data.data ? data.data[attrs[i]] : 0, type: "line"})
-                }
-                // sysTimes = data.data.systime.map(item => item = item.substring(12))
-                sysTimes = data.data.systime;
+        .then(({ data }) => {
+          if (data.code == 200) {
+            let series = [];
+            let sysTimes = [];
+            if (data.data && JSON.stringify(data.data) != "{}") {
+              for (let i = 0; i < attrs.length; i++) {
+                series.push({ data: data.data ? data.data[attrs[i]] : 0, type: "line" })
               }
-
-              let xAxis = {data: sysTimes, type: 'category'} ;
-              let option = { series, xAxis };
-              this.initEChart(option);
+              // sysTimes = data.data.systime.map(item => item = item.substring(12))
+              sysTimes = data.data.systime;
             }
-          })
+
+            let xAxis = { data: sysTimes, type: 'category' };
+            let option = { series, xAxis };
+            this.initEChart(option);
+          }
+        })
     },
-    
-    handleClickRange() {
+
+    handleShowRange() {
       this.rangeDialogVisible = true;
+    },
+    handleRangeSubmit() {
+      // 格林威治时间转为时间戳
+      if (!this.range.startTime || !this.range.endTime) {
+        this.$message.error("请选择时间区间");
+        return;
+      }
+      this.rangeDialogVisible = false;
+      this.getHistory(this.optionData.mapping);
     },
     /**
      * 采样周期
@@ -210,7 +263,7 @@ export default {
     /**
      * 修改 e-chart 大小
      */
-    sizeChange(){
+    sizeChange() {
       let mainRef = this.$refs.chart;
       let width = mainRef.clientWidth;
       let height = mainRef.clientHeight;
@@ -234,11 +287,12 @@ export default {
 <style scoped lang="scss">
 .chart-div {
   position: relative;
-//margin: 10px 20px 20px 10px;
+  //margin: 10px 20px 20px 10px;
   box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
   background-color: #2d3d86;
   border-radius: 4px;
 }
+
 .chart-header {
   position: relative;
   display: flex;
@@ -246,26 +300,29 @@ export default {
   height: 40px;
   padding-left: 10px;
   text-align: right;
-//box-shadow: 0 2px 0px 0 rgba(0, 0, 0, 0.1);
-.title {
-//width: 100%;
-//flex-grow: 1;
-  color: #fff;
-  text-align: center;
-  margin-top: 10px;
-  margin-bottom: 10px;
-  font-size: 18px;
-}
-.tool-right {
-  position: absolute;
-  text-align: center;
 
-  top:4px;
-  right: 4px;
-}
-.tool-item {
-  background: transparent!important;
-  border: 0px solid transparent;
-}
+  //box-shadow: 0 2px 0px 0 rgba(0, 0, 0, 0.1);
+  .title {
+    //width: 100%;
+    //flex-grow: 1;
+    color: #fff;
+    text-align: center;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    font-size: 18px;
+  }
+
+  .tool-right {
+    position: absolute;
+    text-align: center;
+
+    top: 4px;
+    right: 4px;
+  }
+
+  .tool-item {
+    background: transparent !important;
+    border: 0px solid transparent;
+  }
 }
 </style>
