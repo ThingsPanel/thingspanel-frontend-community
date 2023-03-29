8d80d118020cb18a8c062775fe5d3f537253da4b
chaoxiaoshu-mx <leukotrichia@163.com>
2023-03-29 15:29:29 +0800

    修复快捷指南链接地址

--- src/view/content/widgets/list/Widget10.vue
+++ src/view/content/widgets/list/Widget10.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: chaoxiaoshu-mx leukotrichia@163.com
+ * @Date: 2023-02-28 15:11:01
+ * @LastEditors: chaoxiaoshu-mx leukotrichia@163.com
+ * @LastEditTime: 2023-03-29 15:28:43
+ * @FilePath: \ThingsPanel-Backend-Vue\src\view\content\widgets\list\Widget10.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
   <!--begin::List Widget 9-->
   <div class="card card-custom card-stretch gutter-b mb-4.5">
@@ -13,7 +21,7 @@
       </h3>
 
       <div class="width-100 mt-2 mb-5">
-        <a target="_blank" href="http://thingspanel.io/en/docs/quick_start" class="btn mr-3 btn-success active">{{ $t("HOME.QUICK_START") }}</a>
+        <a target="_blank" href="http://thingspanel.io/docs/quick_start" class="btn mr-3 btn-success active">{{ $t("HOME.QUICK_START") }}</a>
         <a target="_blank" href="http://thingspanel.io" class="btn mr-3 btn-success active">{{ $t("HOME.VIEWHELP") }}</a>
         <a target="_blank" href="https://github.com/ThingsPanel" class="btn mr-3 btn-success active">{{ $t("HOME.FEEDBACKQ") }}</a>
       </div>
