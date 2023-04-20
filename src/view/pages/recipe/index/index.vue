43a85bda7ad40b2f2464c749d83ccf3ce17a50ae
joe <chries-rong@protonmail.com>
2023-04-20 21:25:23 +0800

    fix bug

--- src/view/pages/recipe/index/index.vue
+++ src/view/pages/recipe/index/index.vue
@@ -398,6 +398,7 @@ export default {
       
       this.formMertialsData = {}
       this.createMaterialsDialogVisible = false
+      this.materialParams.keyword = ''
     },
     onTasteDialogSubmit() {
       var tmpTasteData = '';
