14173d0d6aff7c2c4d1cd236ae663d41bcb208b0
chaoxiaoshu-mx <leukotrichia@163.com>
2023-07-24 10:01:02 +0800

    feat: 密码修改需要输入旧密码

--- src/view/pages/users/ResetPasswordForm.vue
+++ src/view/pages/users/ResetPasswordForm.vue
@@ -1,37 +1,33 @@
 <template>
-<el-dialog
-    class="el-dark-dialog el-dark-input"
-    :visible.sync="showDialog"
-    width="30%"
-    :title="$t('SYSTEM_MANAGEMENT.USER_MANAGEMENT.CHANGEPASSWORD')"
-    :close-on-click-modal="false"
->
-  <el-form
-      ref="resetPasswordFormRef"
-      label-width="80px"
-      label-position="left"
-      :model="formData"
-      :rules="rules"
+  <el-dialog class="el-dark-dialog el-dark-input" :visible.sync="showDialog" width="30%"
+    :title="$t('SYSTEM_MANAGEMENT.USER_MANAGEMENT.CHANGEPASSWORD')" :close-on-click-modal="false">
+    <el-form ref="resetPasswordFormRef" label-width="80px" label-position="left" :model="formData" :rules="rules"
       hide-required-asterisk>
-    <el-form-item :label="$t('SYSTEM_MANAGEMENT.USER_MANAGEMENT.PASSWORD')" prop="password">
-      <el-input size="medium" v-model="formData.password" show-password></el-input>
-    </el-form-item>
 
-    <el-form-item :label="$t('SYSTEM_MANAGEMENT.USER_MANAGEMENT.CONPASSWORD')" prop="password_confirmation">
-      <el-input size="medium" v-model="formData.password_confirmation" show-password></el-input>
-    </el-form-item>
+      <el-form-item label="原密码" prop="old_password">
+        <el-input size="medium" v-model="formData.old_password" show-password></el-input>
+      </el-form-item>
 
-    <FormAlert :error_message="error_message"></FormAlert>
+      <el-form-item :label="$t('SYSTEM_MANAGEMENT.USER_MANAGEMENT.PASSWORD')" prop="password">
+        <el-input size="medium" v-model="formData.password" show-password></el-input>
+      </el-form-item>
 
-    <el-button size="medium" class="w-100" type="save" @click="handleSubmit">{{$t('SYSTEM_MANAGEMENT.SAVE')}}</el-button>
-  </el-form>
-</el-dialog>
+      <el-form-item :label="$t('SYSTEM_MANAGEMENT.USER_MANAGEMENT.CONPASSWORD')" prop="password_confirmation">
+        <el-input size="medium" v-model="formData.password_confirmation" show-password></el-input>
+      </el-form-item>
+
+      <FormAlert :error_message="error_message"></FormAlert>
+
+      <el-button size="medium" class="w-100" type="save"
+        @click="handleSubmit">{{ $t('SYSTEM_MANAGEMENT.SAVE') }}</el-button>
+    </el-form>
+  </el-dialog>
 </template>
 
 <script>
-import {defineComponent, ref, reactive, watch, computed} from "@vue/composition-api";
-import {user_reset_password} from "@/api/user";
-import {message_success} from "@/utils/helpers";
+import { defineComponent, ref, reactive, watch, computed } from "@vue/composition-api";
+import { user_reset_password } from "@/api/user";
+import { message_success } from "@/utils/helpers";
 import FormAlert from "@/components/common/FormAlert.vue"
 import i18n from "@/core/plugins/vue-i18n.js"
 export default defineComponent({
@@ -48,23 +44,23 @@ export default defineComponent({
       default: false,
     }
   },
-  setup(props, context){
+  setup(props, context) {
     // form表单元素
     let resetPasswordFormRef = ref()
 
     // 父级 props 的计算属性
     let showDialog = computed({
-      get(){
+      get() {
         return !!props.resetPasswordDialogVisible
       },
-      set(val){
+      set(val) {
         context.emit("update:resetPasswordDialogVisible", val)
       },
     });
 
     // 弹窗关闭时重置表单
-    watch(()=> props.resetPasswordDialogVisible, (val)=>{
-      if(val === false) {
+    watch(() => props.resetPasswordDialogVisible, (val) => {
+      if (val === false) {
         resetPasswordFormRef.value.resetFields()
         error_message.value = ""
       }
@@ -72,6 +68,7 @@ export default defineComponent({
 
     // 表单数据
     let formData = reactive({
+      old_password: "",
       password: "",
       password_confirmation: "",
     })
@@ -93,21 +90,25 @@ export default defineComponent({
 
     // 表单验证
     let rules = reactive({
+      old_password: [
+        { required: true, message: "请输入原密码" },
+        { min: 6, message: i18n.t('SYSTEM_MANAGEMENT.USER_MANAGEMENT.PLACEHOLDER7') }
+      ],
       password: [
-        {required:true, message: i18n.t('SYSTEM_MANAGEMENT.USER_MANAGEMENT.PLACEHOLDER6')},
-        {min:6, message: i18n.t('SYSTEM_MANAGEMENT.USER_MANAGEMENT.PLACEHOLDER7')}
+        { required: true, message: i18n.t('SYSTEM_MANAGEMENT.USER_MANAGEMENT.PLACEHOLDER6') },
+        { min: 6, message: i18n.t('SYSTEM_MANAGEMENT.USER_MANAGEMENT.PLACEHOLDER7') }
       ],
       password_confirmation: [
-        {required: true, validator: check_password_confirmation}
+        { required: true, validator: check_password_confirmation }
       ]
     })
 
     // 提交
-    function handleSubmit(){
-      resetPasswordFormRef.value.validate((valid)=>{
-        if(!valid) return;
+    function handleSubmit() {
+      resetPasswordFormRef.value.validate((valid) => {
+        if (!valid) return;
 
-        if(loading.value) return;
+        if (loading.value) return;
         loading.value = true;
 
         // 提交前清除错误
@@ -115,16 +116,16 @@ export default defineComponent({
 
         // 发送请求
         formData.id = props.editUserItem.id;
-        user_reset_password(formData).then(({data})=>{
-          if(data.code === 200) {
+        user_reset_password(formData).then(({ data }) => {
+          if (data.code === 200) {
             message_success(data.message)
 
             // 通知父级关闭 dialog
             context.emit('update:resetPasswordDialogVisible', false)
-          }else{
+          } else {
             error_message.value = data.message
           }
-        }).finally(()=>{
+        }).finally(() => {
           loading.value = false
         })
 
@@ -143,6 +144,4 @@ export default defineComponent({
 })
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
