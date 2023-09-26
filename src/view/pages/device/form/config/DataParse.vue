16e45ad3d999fc0cb61dabe4fd4915f454a11a18
chaoxiaoshu-mx <leukotrichia@163.com>
2023-09-26 11:07:26 +0800

    feat: 设备详情增加modbus协议

--- src/view/pages/device/form/config/DataParse.vue
+++ src/view/pages/device/form/config/DataParse.vue
@@ -1,24 +1,79 @@
 <template>
     <div>
-        <el-form ref="configForm" :model="formData" :rules="formRule" label-width="260px">
-            <el-form-item v-for="(attr, index) in attrs" :key="index" :label="attr.label" :prop="attr.dataKey">
+        <el-form ref="configForm" :model="formData" :rules="formRules" label-width="260px">
+            <el-form-item v-for="(attr, index) in formAttrs" :key="index" :label="attr.label" :prop="attr.dataKey">
 
-                <el-input style="width: 80%" v-if="attr.type == 'input' && attr.validate.type != 'number'"
+                <el-input style="width: 80%" v-if="attr.type === 'input' && attr.validate.type !== 'number'"
                     v-model="formData[attr.dataKey]" :placeholder="attr.placeholder"></el-input>
 
-                <el-input-number style="width: 80%" v-if="attr.type == 'input' && attr.validate.type == 'number'"
+                <el-input-number style="width: 80%" v-else-if="attr.type === 'input' && attr.validate.type === 'number'"
                     v-model="formData[attr.dataKey]" :placeholder="attr.placeholder"></el-input-number>
 
-                <el-select style="width: 80%" v-if="attr.type == 'select'" v-model="formData[attr.dataKey]">
+                <el-select style="width: 80%" v-else-if="attr.type === 'select'" v-model="formData[attr.dataKey]">
                     <el-option v-for="(option, index) in attr.options" :key="index" :label="option.label"
                         :value="option.value"></el-option>
                 </el-select>
+
+                <el-table style="width: 80%" v-if="attr.type === 'table'" :data.sync="formData[attr.dataKey]">
+                    <!-- 内容列 start -->
+                    <el-table-column v-for="(item, index) in attr.array" 
+                        :key="index" 
+                        :prop="item.dataKey" 
+                        :label="item.label"
+                        width="140">
+                        <template slot-scope="scope">
+                            
+                            <el-form ref="tableRef" :model="scope.row" :rules="item.rules" :error="item.validate.message">
+                                <el-form-item :prop="item.dataKey">
+                                    <template v-if="item.type==='input'">
+                                        <el-input-number size="small" v-if="item.validate.type==='number'" 
+                                            v-model="scope.row[item.dataKey]" :placeholder="item.placeholder"/>
+
+                                        <el-input size="small" v-else
+                                            v-model="scope.row[item.dataKey]" :placeholder="item.placeholder"/>
+                                        <span slot="error" class="el-form-input_tip el-form-item__error">
+                                            {{item.validate.message}}
+                                        </span>
+                                    </template> 
+                                    
+                                    <template v-else-if="item.type==='select'">
+                                        <el-select size="small" v-model="scope.row[item.dataKey]" :placeholder="item.placeholder">
+                                            <el-option v-for="(option, index) in item.options" :key="index" 
+                                                :label="option.label" :value="option.value"/>
+                                        </el-select>
+                                        <span slot="error" class="el-form-input_tip el-form-item__error">
+                                            {{item.validate.message}}
+                                        </span>
+                                    </template>
+                                </el-form-item>
+                            </el-form>
+                        </template>
+                    </el-table-column>
+                    <!-- 内容列 end -->
+
+                    <!-- 操作列 start -->
+                    <el-table-column prop="handle" label="操作" width="100" align="right">
+                        <!-- 操作列表头 -->
+                        <template slot="header" slot-scope="scope">
+                            <el-button size="small" @click="handleAdd(attr)">新增</el-button>
+                        </template>
+                        <!-- 操作列内容 -->
+                        <template slot-scope="scope">
+                            <!-- <el-button size="mini" @click="handleEdit(attr, scope.$index, scope.row)">编辑</el-button> -->
+                            <el-button size="mini" @click="handleDel(attr, scope.$index, scope.row)">删除</el-button></el-button>
+                        </template>
+                    </el-table-column>
+                    <!-- 操作列 end -->
+
+                </el-table>
             </el-form-item>
         </el-form>
     </div>
 </template>
 
 <script>
+
+
 export default {
     components: {},
     props: {
@@ -35,43 +90,158 @@ export default {
     },
     data() {
         return {
+            // formData: {},
             formAttr: [],
             formRule: {}
         }
     },
     computed: {
+        formAttrs: {
+            get() {
+                let attrs = JSON.parse(JSON.stringify(this.attrs));
+                attrs.forEach(attr => {
+                    if (!attr.array) {
+                        attr.rules = {};
+                        attr.rules[attr.dataKey] = [attr.validate];
+                    } else {
+                        attr.array.forEach(tableAttr => {
+                            tableAttr.rules = {};
+                            tableAttr.rules[tableAttr.dataKey] = [tableAttr.validate];
+                        })
+                    }
+                })
+                return attrs;
+            }
+        },
+        formRules: {
+            get() {
+                let rules = {};
+                this.attrs.forEach(attr => {
+                    if (attr.type !== "table") {
+                        rules[attr.dataKey] = [attr.validate]
+                    }
+                })
+                return rules;
+            }
+        },
         formData: {
             get() {
-                return this.data
-            },
-            set(val) {
-                this.$emit('update:data', val)
+                console.log("computed.data", JSON.stringify(this.data))
+                if (this.data && JSON.stringify(this.data) !== "{}") {
+                    return this.data
+                } else {
+                    let obj = {};
+                    this.attrs.forEach(attr => {
+                        console.log("computed.forEach.attr", attr)
+                        if (attr.type === "input") {
+                            // 输入框
+                            obj[attr.dataKey] = attr.validate.type === "number" ? 0 : "";
+                        } else if (attr.type === "table") {
+                            // 表格
+                            obj[attr.dataKey] = [];
+                        }
+                    })
+                    console.log("computed.obj", obj)
+                    return obj;
+                }
             }
         }
     },
+    watch: {
+        formData: {
+            handler(val) {
+                console.log("watch.formData", val)
+                if (JSON.stringify(val) !== JSON.stringify(this.data)){
+                    this.$emit('update:data', val)
+                }
+            },
+            deep: true,
+            immediate: true
+        }
+    },
     mounted() {
+        console.log("DataParse.attrs", this.attrs)
         this.formRule = this.getFormRule(this.attrs);
+        // this.getFormRule(this.attrs);
+        console.log("DataParse.formRule", this.formRule)
+
     },
     methods: {
         getFormRule(formAttr) {
+            console.log("getFormRule.formAttr", formAttr)
             let rules = {};
             formAttr.forEach(attr => {
-                let rule = {};
-                rule.required = attr.validate.required;
-                rule.message = attr.validate.message;
-                if (attr.validate.type) {
-                    rule.type = attr.validate.type;
+                if (!attr.array) {
+                    rules[attr.dataKey] = [attr.validate]
                 }
-                rules[attr.dataKey] = [rule]
             })
             return rules;
         },
+        handleAdd(attr) {
+            console.log("handleAdd1", this.formData, attr)
+            // 根据attr.array生成一行数据
+            let newLine = {};
+            let obj = {};
+            attr.array.forEach(item => {
+                obj[item.dataKey] = "";
+            })
+            this.formData[attr.dataKey].push(obj)
+            console.log("handleAdd2", this.formData)
+            
+        },
+        handleEdit(attr,index, item) {
+            console.log("handleEdit", attr, index, item)
+        },
+        handleDel(attr,index, item) {
+            this.formData[attr.dataKey].splice(index, 1)
+            console.log("handleDel", index, this.formData)
+
+        },
         validate(cb) {
-            this.$refs.configForm.validate(valid => {
-                cb(valid)
-            });
+            console.log("this.$refs.tableRef", this.$refs.tableRef)
+            try {
+                this.$refs.configForm.validate(valid => {
+                    if (!valid) {
+                        throw new Error("");
+                    }
+                });
+                if (!this.$refs.tableRef || this.$refs.tableRef.length === 0) {
+                    throw new Error("");
+                }
+                for (let i = 0; i < this.$refs.tableRef.length; i++) {
+                    const ref = this.$refs.tableRef[i];
+                    ref.validate(valid => {
+                        if (!valid) {
+                            throw new Error("");
+                        }
+                    })
+                }
+                cb(true);
+            } catch (err) {
+                console.log(err)
+                cb(false);
+            }
         }
     }
 }
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.el-table--fit {
+    border-right: 1px solid #344a9a;
+    border-bottom: 0;
+    border-left: 1px solid #344a9a;
+}
+::v-deep .el-table .el-table__cell {
+    padding-top: 0px;
+}
+::v-deep .el-form-item__content {
+    line-height: 66px;
+}
+::v-deep .el-form-item__error {
+    // position:static!important;
+    color: #F56C6C!important;
+    padding: 0px!important;
+    top: 56px;
+    left: 4px;
+}
+</style>
--- src/view/pages/device/form/config/index.vue
+++ src/view/pages/device/form/config/index.vue
@@ -1,61 +1,62 @@
 <!-- 设备配置 -->
 <template>
   <el-dialog class="el-dark-dialog el-dark-input" :title="$t('DEVICE_MANAGEMENT.DEVICE_DETAIL')" width="60%"
-             :close-on-click-modal="false" :close-on-press-escape="false" :show-close="false" :append-to-body="true"
-             :visible.sync="dialogVisible" @close="handleClose">
+    :close-on-click-modal="false" :close-on-press-escape="false" :show-close="false" :append-to-body="true"
+    :visible.sync="dialogVisible" @close="handleClose">
     <div class="container-fluid">
       <el-tabs v-model="activeName">
         <el-tab-pane v-for="item in tabList" :key="item.value" :label="item.label" :name="item.value"></el-tab-pane>
       </el-tabs>
 
       <!-- 数据解析 -->
-      <data-parse ref="dataParse" v-if="activeName=='configParse'" :data.sync="formData" :attrs="formAttr"></data-parse>
+      <data-parse ref="dataParse" v-if="activeName == 'configParse'" :data.sync="formData" :attrs="formAttr"></data-parse>
 
       <!-- 设备概览 -->
-      <device-attribute v-else-if="activeName=='deviceAttribute'" :data.sync="attrFormData" :device="device" :wvpDevice="wvpDeviceList" :attributeCard="deviceAttributeCardList"></device-attribute>
+      <device-attribute v-else-if="activeName == 'deviceAttribute'" :data.sync="attrFormData" :device="device"
+        :wvpDevice="wvpDeviceList" :attributeCard="deviceAttributeCardList"></device-attribute>
 
       <!-- 设备属性 -->
-      <attribute v-else-if="activeName=='attribute'" :data.sync="attrFormData" :device="device" :wvpDevice="wvpDeviceList"></attribute>
+      <attribute v-else-if="activeName == 'attribute'" :data.sync="attrFormData" :device="device"
+        :wvpDevice="wvpDeviceList"></attribute>
 
       <!-- 运维信息 -->
-      <running-info v-else-if="activeName=='runningStatus'" :data.sync="runningFormData"></running-info>
+      <running-info v-else-if="activeName == 'runningStatus'" :data.sync="runningFormData"></running-info>
 
       <!-- 事件 -->
-      <event v-else-if="activeName=='event'" :data.sync="eventFormData" :device="device"></event>
+      <event v-else-if="activeName == 'event'" :data.sync="eventFormData" :device="device"></event>
 
       <!-- 命令 -->
-      <command v-else-if="activeName=='command'" :data.sync="commandFormData" :device="device"></command>
+      <command v-else-if="activeName == 'command'" :data.sync="commandFormData" :device="device"></command>
     </div>
 
     <span slot="footer" class="dialog-footer">
-        <el-button type="cancel" @click="handleClose">{{ $t('DEVICE_MANAGEMENT.DEVICE_CONFIG.CANCEL') }}</el-button>
-        <el-button type="save" @click="handleSubmit">{{ $t('DEVICE_MANAGEMENT.DEVICE_CONFIG.CONFIRM') }}</el-button>
-      </span>
+      <el-button type="cancel" @click="handleClose">{{ $t('DEVICE_MANAGEMENT.DEVICE_CONFIG.CANCEL') }}</el-button>
+      <el-button type="save" @click="handleSubmit">{{ $t('DEVICE_MANAGEMENT.DEVICE_CONFIG.CONFIRM') }}</el-button>
+    </span>
 
   </el-dialog>
-  
 </template>
 
 <script>
 import ModbusAPI from "@/api/modbus"
 import ProtocolPluginAPI from "@/api/protocolPlugin"
 import { currentValueDetail } from "@/api/device"
-import {message_success} from "@/utils/helpers";
+import { message_success } from "@/utils/helpers";
 import DataParse from "./DataParse.vue"
 import DeviceAttribute from './DeviceAttribute'
-import RunningInfo from "./RunningInfo.vue"
-import Event from "./Event.vue"
-import Command from "./Command.vue"
-import Attribute from "./attribute.vue"
+import RunningInfo from "./RunningInfo"
+import Event from "./Event"
+import Command from "./Command"
+import Attribute from "./attribute"
 import i18n from "@/core/plugins/vue-i18n.js"
 const tabList = [
-        { value: "configParse", label: i18n.t('DEVICE_MANAGEMENT.DEVICE_CONFIG.DATAANALYSIS')},
-        { value: "deviceAttribute", label: i18n.t('DEVICE_MANAGEMENT.DEVICE_CONFIG.OVERVIEW')},
-        { value: "attribute", label: i18n.t('DEVICE_MANAGEMENT.DEVICE_CONFIG.PROPERTIES_REPORT') },
-        { value: "runningStatus", label: i18n.t('DEVICE_MANAGEMENT.DEVICE_CONFIG.OPERATION')},
-        { value: "event", label: i18n.t('DEVICE_MANAGEMENT.DEVICE_CONFIG.EVENT_REPORT') },
-        { value: "command", label: i18n.t('DEVICE_MANAGEMENT.DEVICE_CONFIG.COMMAND_DELIVERY')}
-      ]
+  { value: "configParse", label: i18n.t('DEVICE_MANAGEMENT.DEVICE_CONFIG.DATAANALYSIS') },
+  { value: "deviceAttribute", label: i18n.t('DEVICE_MANAGEMENT.DEVICE_CONFIG.OVERVIEW') },
+  { value: "attribute", label: i18n.t('DEVICE_MANAGEMENT.DEVICE_CONFIG.PROPERTIES_REPORT') },
+  { value: "runningStatus", label: i18n.t('DEVICE_MANAGEMENT.DEVICE_CONFIG.OPERATION') },
+  { value: "event", label: i18n.t('DEVICE_MANAGEMENT.DEVICE_CONFIG.EVENT_REPORT') },
+  { value: "command", label: i18n.t('DEVICE_MANAGEMENT.DEVICE_CONFIG.COMMAND_DELIVERY') }
+]
 export default {
   name: "DeviceDetail",
   components: {
@@ -73,7 +74,7 @@ export default {
     },
     device: {
       type: [Object],
-      default: () => {return {}}
+      default: () => { return {} }
     }
   },
   data() {
@@ -100,8 +101,8 @@ export default {
         if (newValue) {
           console.log("====DeviceConfigForm.newValue", this.device)
           this.location = this.device.location ? this.device.location : "";
-          let additionalInfo = (this.device.additional_info && this.device.additional_info!="null") ? JSON.parse(this.device.additional_info) :
-              {runningInfo: {thresholdTime: 0}};
+          let additionalInfo = (this.device.additional_info && this.device.additional_info != "null") ? JSON.parse(this.device.additional_info) :
+            { runningInfo: { thresholdTime: 0 } };
 
           this.attrFormData = {
             d_id: this.device.d_id,
@@ -112,27 +113,114 @@ export default {
 
           this.runningFormData = additionalInfo.runningInfo ? additionalInfo.runningInfo : {};
           this.formData = {};
-          if (JSON.stringify(this.device) == "{}" || this.device == "") {return;}
+          if (JSON.stringify(this.device) == "{}" || this.device == "") { return; }
           if (this.device.device_type == "3" || (this.device.device_type == "1" && this.device.protocol != "mqtt")) {
             // 子设备所有协议都要数据解析，直连设备所有自定义协议(mqtt协议之外)都要数据解析
             this.tabList = JSON.parse(JSON.stringify(tabList));
             this.activeName = "configParse";
             let deviceType = this.device.device_type == "1" ? "1" : "2";
             // 获取表单的属性
-            ModbusAPI.getFormAttr({ protocol_type: this.device.protocol, device_type: deviceType})
-                .then(({data}) => {
-                  if (data.code == 200 && data.data && data.data.config) {
-                    this.formAttr = data.data.config;
-                    if (this.device.protocol_config != "{}"
-                        && this.device.protocol_config != ""
-                        && this.device.protocol_config != undefined) {
-                      this.formData = JSON.parse(this.device.protocol_config);
+            ModbusAPI.getFormAttr({ protocol_type: this.device.protocol, device_type: deviceType })
+              .then(({ data }) => {
+                if (data.code == 200 && data.data && data.data.config) {
+                  const testFormAttr = [
+                    {
+                      "dataKey": "temp",
+                      "label": "读取策略(秒)",
+                      "placeholder": "请输入时间间隔，单位s",
+                      "type": "input",
+                      "validate": {
+                        "message": "读取策略不能为空",
+                        "required": true,
+                        "rules": "/^\\d{1,}$/",
+                        "type": "number"
+                      }
+                    },
+                    {
+                      "type": "table",
+                      "label": "属性列表",
+                      "dataKey": "table1",
+                      "array": [
+                        {
+                          "dataKey": "Interval1",
+                          "label": "读取策略1",
+                          "placeholder": "请输入时间间隔",
+                          "type": "input",
+                          "validate": {
+                            "message": "读取策略不能为空",
+                            "required": true,
+                            "rules": "/^\\d{1,}$/",
+                            "type": "input"
+                          },
+                          "errMsg": "请输入读取策略不能为空",
+                          rules: {
+                            Interval1: [
+                              { required: true, message: "请输入活动名称1", trigger: "blur" },
+                            ],
+                          }
+                        },
+                        {
+                          "dataKey": "Interval2",
+                          "label": "读取策略2(秒)",
+                          "placeholder": "请输入时间间隔，单位s",
+                          "type": "input",
+                          "errMsg": "请输入读取策略不能为空",
+                          "validate": {
+                            "message": "读取策略不能为空",
+                            "required": true,
+                            "rules": "/^\\d{1,}$/",
+                            "type": "number"
+                          },
+                          rules: {
+                            Interval2: [
+                              { required: true, message: "请输入活动名称2", trigger: "blur" ,min: 2},
+                            ],
+                          }
+                        },
+                        {
+                          "dataKey": "Interval3",
+                          "label": "读取策略3(秒)",
+                          "placeholder": "请输入时间间隔，单位s",
+                          "type": "input",
+                          "errMsg": "请输入读取策略不能为空",
+                          "validate": {
+                            "message": "读取策略不能为空",
+                            "required": true,
+                            "rules": "/^\\d{1,}$/",
+                            "type": "number"
+                          }
+                        },
+                        {
+                          "dataKey": "Interval4",
+                          "label": "读取策略4(秒)",
+                          "placeholder": "请输入时间间隔，单位s",
+                          "type": "input",
+                          "errMsg": "请输入读取策略不能为空",
+                          "validate": {
+                            "message": "读取策略不能为空",
+                            "required": true,
+                            "rules": "/^\\d{1,}$/",
+                            "type": "number"
+                          }
+                        }
+                      ]
                     }
-                  } else {
-                    this.tabList.splice(0, 1);
-                    this.activeName = "deviceAttribute";
+                  ]
+                  this.formAttr = data.data.config;
+                  console.log("data.data.config", data.data.config)
+                  // this.formAttr = testFormAttr;
+                  console.log("this.device.protocol_config", this.device.protocol_config)
+
+                  if (this.device.protocol_config != "{}"
+                    && this.device.protocol_config != ""
+                    && this.device.protocol_config != undefined) {
+                    this.formData = JSON.parse(this.device.protocol_config);
                   }
-                })
+                } else {
+                  this.tabList.splice(0, 1);
+                  this.activeName = "deviceAttribute";
+                }
+              })
           } else {
             this.tabList = JSON.parse(JSON.stringify(tabList));
             // const configParseIndex = this.tabList.findIndex(item => item.value === "configParse");
@@ -160,8 +248,8 @@ export default {
       this.$emit("update:dialogVisible", false);
     },
     getWVPDeviceList() {
-      ProtocolPluginAPI.getWVPDevices({"id": this.device.id,"count": "9999","page":"1"})
-        .then(({data}) => {
+      ProtocolPluginAPI.getWVPDevices({ "id": this.device.id, "count": "9999", "page": "1" })
+        .then(({ data }) => {
           console.log("====getWVPDeviceList", data)
           if (data.code == 200) {
             this.wvpDeviceList = data.data.list ? data.data.list : [];
@@ -169,7 +257,7 @@ export default {
         })
     },
     getDeviceAttributeCardList() {
-      currentValueDetail({"device_id": this.device.id})
+      currentValueDetail({ "device_id": this.device.id })
         .then(data => {
           console.debug("====getDeviceAttributeCardList", data)
           if (data.data.code == 200) {
@@ -179,10 +267,10 @@ export default {
     },
     handleSubmit() {
       console.log("====DeviceConfigForm", this.formData)
-
+      
       const submit = () => {
         let device = this.device;
-        let additionalInfo = (this.device.additional_info && this.device.additional_info!="null") ? JSON.parse(this.device.additional_info) : {};
+        let additionalInfo = (this.device.additional_info && this.device.additional_info != "null") ? JSON.parse(this.device.additional_info) : {};
         additionalInfo.runningInfo = this.runningFormData;
         additionalInfo.video_address = this.attrFormData.subDeviceVideoAddress;
         let config = {
@@ -191,18 +279,18 @@ export default {
           additional_info: JSON.stringify(additionalInfo),
           location: this.attrFormData.location,
           sub_device_addr: this.attrFormData.subDeviceAddress,
-          protocol_config: JSON.stringify({DeviceId: device.id, AccessToken: device.token, ...this.formData})
+          protocol_config: JSON.stringify({ DeviceId: device.id, AccessToken: device.token, ...this.formData })
         }
         console.log("====DeviceConfigForm.config", config, this.attrFormData)
 
         ModbusAPI.updateDeviceConfig(config)
-            .then(({data}) => {
-              if (data.code == 200) {
-                message_success("配置成功！");
-                this.handleClose();
-                this.$emit("submit");
-              }
-            })
+          .then(({ data }) => {
+            if (data.code == 200) {
+              message_success("配置成功！");
+              this.handleClose();
+              this.$emit("submit");
+            }
+          })
       }
       if (this.$refs["dataParse"]) {
         this.$refs["dataParse"].validate((valid) => {
@@ -219,6 +307,4 @@ export default {
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
