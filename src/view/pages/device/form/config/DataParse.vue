a5e0f740ba75c5a65ee86a5bd0227dcb413d835e
chaoxiaoshu-mx <leukotrichia@163.com>
2023-09-27 17:18:31 +0800

    fix: 优化设备详情

--- src/view/pages/device/form/config/DataParse.vue
+++ src/view/pages/device/form/config/DataParse.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
-        <el-form ref="configForm" :model="formData" :rules="formRules" label-width="260px">
-            <el-form-item v-for="(attr, index) in formAttrs" :key="index" :label="attr.label" :prop="attr.dataKey">
+    <div style="margin: 20px 0">
+        <el-form ref="configForm" :model="formData" :rules="formRules" label-width="260px" >
+            <el-form-item v-for="(attr, index) in attrs" :key="index" :label="attr.label" :prop="attr.dataKey">
 
                 <el-input style="width: 80%" v-if="attr.type === 'input' && attr.validate.type !== 'number'"
                     v-model="formData[attr.dataKey]" :placeholder="attr.placeholder"></el-input>
@@ -14,68 +14,20 @@
                         :value="option.value"></el-option>
                 </el-select>
 
-                <el-table style="width: 80%" v-if="attr.type === 'table'" :data.sync="formData[attr.dataKey]">
-                    <!-- 内容列 start -->
-                    <el-table-column v-for="(item, index) in attr.array" 
-                        :key="index" 
-                        :prop="item.dataKey" 
-                        :label="item.label"
-                        width="140">
-                        <template slot-scope="scope">
-                            
-                            <el-form ref="tableRef" :model="scope.row" :rules="item.rules" :error="item.validate.message">
-                                <el-form-item :prop="item.dataKey">
-                                    <template v-if="item.type==='input'">
-                                        <el-input-number size="small" v-if="item.validate.type==='number'" 
-                                            v-model="scope.row[item.dataKey]" :placeholder="item.placeholder"/>
-
-                                        <el-input size="small" v-else
-                                            v-model="scope.row[item.dataKey]" :placeholder="item.placeholder"/>
-                                        <span slot="error" class="el-form-input_tip el-form-item__error">
-                                            {{item.validate.message}}
-                                        </span>
-                                    </template> 
-                                    
-                                    <template v-else-if="item.type==='select'">
-                                        <el-select size="small" v-model="scope.row[item.dataKey]" :placeholder="item.placeholder">
-                                            <el-option v-for="(option, index) in item.options" :key="index" 
-                                                :label="option.label" :value="option.value"/>
-                                        </el-select>
-                                        <span slot="error" class="el-form-input_tip el-form-item__error">
-                                            {{item.validate.message}}
-                                        </span>
-                                    </template>
-                                </el-form-item>
-                            </el-form>
-                        </template>
-                    </el-table-column>
-                    <!-- 内容列 end -->
-
-                    <!-- 操作列 start -->
-                    <el-table-column prop="handle" label="操作" width="100" align="right">
-                        <!-- 操作列表头 -->
-                        <template slot="header" slot-scope="scope">
-                            <el-button size="small" @click="handleAdd(attr)">新增</el-button>
-                        </template>
-                        <!-- 操作列内容 -->
-                        <template slot-scope="scope">
-                            <!-- <el-button size="mini" @click="handleEdit(attr, scope.$index, scope.row)">编辑</el-button> -->
-                            <el-button size="mini" @click="handleDel(attr, scope.$index, scope.row)">删除</el-button></el-button>
-                        </template>
-                    </el-table-column>
-                    <!-- 操作列 end -->
-
-                </el-table>
+                <el-button type="save" size="medium" v-if="attr.type === 'table'" @click="handleShowTableDialog(attr)">{{ attr.label }}</el-button>
+                
             </el-form-item>
         </el-form>
+       
+        <data-parse-table :visible.sync="dialogVisible" :attrs="currentTableAttr" :data.sync="currentTableData" @submit="handleTableSubmit"></data-parse-table>
     </div>
 </template>
 
 <script>
-
+import DataParseTable from './DataParseTable.vue';
 
 export default {
-    components: {},
+    components: { DataParseTable },
     props: {
         data: {
             type: Object,
@@ -90,35 +42,28 @@ export default {
     },
     data() {
         return {
-            // formData: {},
-            formAttr: [],
-            formRule: {}
+            dialogVisible: false,
+            currentTableAttr: {},
+            currentTableData: []
         }
     },
     computed: {
-        formAttrs: {
-            get() {
-                let attrs = JSON.parse(JSON.stringify(this.attrs));
-                attrs.forEach(attr => {
-                    if (!attr.array) {
-                        attr.rules = {};
-                        attr.rules[attr.dataKey] = [attr.validate];
-                    } else {
-                        attr.array.forEach(tableAttr => {
-                            tableAttr.rules = {};
-                            tableAttr.rules[tableAttr.dataKey] = [tableAttr.validate];
-                        })
-                    }
-                })
-                return attrs;
-            }
-        },
         formRules: {
             get() {
                 let rules = {};
                 this.attrs.forEach(attr => {
                     if (attr.type !== "table") {
                         rules[attr.dataKey] = [attr.validate]
+                        if (attr.validate.rules) {
+                            let trimmedRegexString = attr.validate.rules.slice(1, -1);
+                            let reg = new RegExp(trimmedRegexString)
+                            rules[attr.dataKey].push({
+                                pattern: reg, 
+                                message: attr.validate.message, 
+                                trigger: 'blur'
+                            })
+                        }
+                        
                     }
                 })
                 return rules;
@@ -159,63 +104,26 @@ export default {
             immediate: true
         }
     },
-    mounted() {
-        console.log("DataParse.attrs", this.attrs)
-        this.formRule = this.getFormRule(this.attrs);
-        // this.getFormRule(this.attrs);
-        console.log("DataParse.formRule", this.formRule)
-
-    },
     methods: {
-        getFormRule(formAttr) {
-            console.log("getFormRule.formAttr", formAttr)
-            let rules = {};
-            formAttr.forEach(attr => {
-                if (!attr.array) {
-                    rules[attr.dataKey] = [attr.validate]
-                }
-            })
-            return rules;
+        handleShowTableDialog(attr) {
+            console.log("handleShowTableDialog", attr)
+            this.currentTableAttr = attr;
+            this.currentTableData = this.formData[attr.dataKey];
+            this.dialogVisible = true;
         },
-        handleAdd(attr) {
-            console.log("handleAdd1", this.formData, attr)
-            // 根据attr.array生成一行数据
-            let newLine = {};
-            let obj = {};
-            attr.array.forEach(item => {
-                obj[item.dataKey] = "";
-            })
-            this.formData[attr.dataKey].push(obj)
-            console.log("handleAdd2", this.formData)
-            
-        },
-        handleEdit(attr,index, item) {
-            console.log("handleEdit", attr, index, item)
-        },
-        handleDel(attr,index, item) {
-            this.formData[attr.dataKey].splice(index, 1)
-            console.log("handleDel", index, this.formData)
-
+        handleTableSubmit(data) {
+            console.log("handleTableSubmit", data)
+            this.formData[this.currentTableAttr.dataKey] = data;
         },
         validate(cb) {
             console.log("this.$refs.tableRef", this.$refs.tableRef)
+            console.log("dataparse.formData", this.formData)
             try {
                 this.$refs.configForm.validate(valid => {
                     if (!valid) {
                         throw new Error("");
                     }
                 });
-                if (!this.$refs.tableRef || this.$refs.tableRef.length === 0) {
-                    throw new Error("");
-                }
-                for (let i = 0; i < this.$refs.tableRef.length; i++) {
-                    const ref = this.$refs.tableRef[i];
-                    ref.validate(valid => {
-                        if (!valid) {
-                            throw new Error("");
-                        }
-                    })
-                }
                 cb(true);
             } catch (err) {
                 console.log(err)
@@ -226,22 +134,10 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-.el-table--fit {
-    border-right: 1px solid #344a9a;
-    border-bottom: 0;
-    border-left: 1px solid #344a9a;
+
+
+::v-deep .el-form-item {
+    margin-bottom: 0px;
 }
-::v-deep .el-table .el-table__cell {
-    padding-top: 0px;
-}
-::v-deep .el-form-item__content {
-    line-height: 66px;
-}
-::v-deep .el-form-item__error {
-    // position:static!important;
-    color: #F56C6C!important;
-    padding: 0px!important;
-    top: 56px;
-    left: 4px;
-}
+
 </style>
---
+++ src/view/pages/device/form/config/DataParseTable.vue
@@ -0,0 +1,192 @@
+<template>
+    <el-dialog class="el-dark-dialog el-dark-input" title="配置" width="80%"
+        :close-on-click-modal="false" :close-on-press-escape="false" :append-to-body="true" :visible.sync="dialogVisible"
+        @close="dialogVisible = false">
+
+        <el-table :data.sync="tableData">
+            <!-- 内容列 start -->
+            <el-table-column v-for="(item, index) in tableAttrs.array" :key="index" :prop="item.dataKey"
+                :label="item.label" width="auto">
+                <template slot-scope="scope">
+
+                    <!-- <el-form ref="tableRef" :model="scope.row" :rules="item.rules" :error="item.validate.message"> -->
+                    <el-form ref="tableRef" :model="scope.row" :rules="item.rules">
+                        <el-form-item :prop="item.dataKey">
+                            <template v-if="item.type === 'input'">
+                                <el-input-number size="small" v-if="item.validate.type === 'number'"
+                                    v-model="scope.row[item.dataKey]" :placeholder="item.placeholder" />
+
+                                <el-input size="small" v-else v-model="scope.row[item.dataKey]"
+                                    :placeholder="item.placeholder" />
+                                <!-- <span slot="error" class="el-form-input_tip el-form-item__error">
+                                    {{ item.validate.message }}
+                                </span> -->
+                            </template>
+
+                            <template v-else-if="item.type === 'select'">
+                                <el-select size="small" v-model="scope.row[item.dataKey]" :placeholder="item.placeholder">
+                                    <el-option v-for="(option, index) in item.options" :key="index" :label="option.label"
+                                        :value="option.value" />
+                                </el-select>
+                                <span slot="error" class="el-form-input_tip el-form-item__error">
+                                    {{ item.validate.message }}
+                                </span>
+                            </template>
+                        </el-form-item>
+                    </el-form>
+                </template>
+            </el-table-column>
+            <!-- 内容列 end -->
+
+            <!-- 操作列 start -->
+            <el-table-column prop="handle" label="操作" width="100" align="right">
+                <!-- 操作列表头 -->
+                <template slot="header" slot-scope="scope">
+                    <el-button type="save" size="small" @click="handleAdd">新增</el-button>
+                </template>
+                <!-- 操作列内容 -->
+                <template slot-scope="scope">
+                    <!-- <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button> -->
+                    <el-button type="danger" size="mini" @click="handleDel(scope.$index, scope.row)">删除</el-button>
+                </template>
+            </el-table-column>
+            <!-- 操作列 end -->
+
+        </el-table>
+
+        <span slot="footer" class="dialog-footer">
+            <el-button type="cancel" @click="dialogVisible = false">{{ $t('DEVICE_MANAGEMENT.DEVICE_CONFIG.CANCEL')
+            }}</el-button>
+            <el-button type="save" @click="handleSubmit(tableData)">{{ $t('DEVICE_MANAGEMENT.DEVICE_CONFIG.CONFIRM')
+            }}</el-button>
+        </span>
+    </el-dialog>
+</template>
+
+<script>
+export default {
+    components: {},
+    props: {
+        visible: {
+            type: [Boolean],
+            default: false
+        },
+        attrs: {
+            type: [Object],
+            default: () => ({})
+        },
+        data: {
+            type: [Array],
+            default: () => ([])
+        }
+    },
+    data() {
+        return {
+        }
+    },
+    computed: {
+        dialogVisible: {
+            get() {
+                return this.visible;
+            },
+            set(val) {
+                this.$emit("update:visible", val);
+            }
+        },
+        tableAttrs: {
+            get() {
+                let attrs = JSON.parse(JSON.stringify(this.attrs));
+                attrs.array && attrs.array.forEach(item => {
+                    item.rules = {};
+                    item.rules[item.dataKey] = [ item.validate ];
+                    if (item.validate.rules) {
+                        let trimmedRegexString = item.validate.rules.slice(1, -1);
+                        let reg = new RegExp(trimmedRegexString)
+                        item.rules[item.dataKey].push(
+                            { 
+                                pattern: reg, 
+                                message: item.validate.message, 
+                                trigger: 'blur'
+                            })
+                    }
+                })
+                console.log("attrs", attrs)
+                return attrs;
+                
+            }
+        },
+        tableData: {
+            get() {
+                return JSON.parse(JSON.stringify(this.data));
+            }
+        }
+    },
+    methods: {
+        /**
+         * 添加一行
+         */
+        handleAdd() {
+            // 根据attr.array生成一行数据
+            let newLine = {};
+            this.attrs.array.forEach(item => {
+                newLine[item.dataKey] = "";
+            })
+            this.tableData.push(newLine)
+        },
+        handleEdit(attr,index, item) {
+            console.log("handleEdit", attr, index, item)
+        },
+        /**
+         * 删除一行
+         */
+        handleDel(index, item) {
+            this.tableData[this.attrs.dataKey].splice(index, 1)
+            console.log("handleDel", index, this.tableData)
+
+        },
+        /**
+         * 提交
+         */
+        handleSubmit(data) {
+            
+            try {
+                let valid = true;
+                for (let i = 0; i < this.$refs.tableRef.length; i++) {
+                    this.$refs.tableRef[i].validate(valid => {
+                        if (!valid) {
+                            valid = false;
+                            throw new Error("");
+                        }
+                    })
+                }
+                if (valid) {
+                    console.log("handleSubmit1", this.tableData)
+                    // this.$emit("update:data", this.tableData)
+                    this.$emit("submit", JSON.parse(JSON.stringify(this.tableData)))
+                    this.dialogVisible = false;
+                }
+            } catch(err) {}
+        },
+    }
+}
+</script>
+<style lang="scss" scoped>
+.el-table--fit {
+    border-right: 1px solid #344a9a;
+    border-left: 1px solid #344a9a;
+}
+::v-deep .el-table .el-table__cell {
+    padding-top: 0px;
+}
+::v-deep .el-form-item__content {
+    line-height: 66px;
+}
+::v-deep .el-form-item__error {
+    // position:static!important;
+    color: #F56C6C!important;
+    padding: 0px!important;
+    top: calc(100% - 14px)!important;
+    left: 0px;
+    width: 100%;
+}
+</style>
