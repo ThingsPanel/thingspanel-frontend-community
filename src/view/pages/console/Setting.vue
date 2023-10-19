b7ba085ff1312ba6bb27a1b00f82440cf379f59d
chaoxiaoshu-mx <leukotrichia@163.com>
2023-10-19 12:25:07 +0800

    feat: 看板增加配置功能

--- src/view/pages/console/Setting.vue
+++ src/view/pages/console/Setting.vue
@@ -2,7 +2,7 @@
  * @Author: chaoxiaoshu-mx leukotrichia@163.com
  * @Date: 2023-10-13 09:35:09
  * @LastEditors: chaoxiaoshu-mx leukotrichia@163.com
- * @LastEditTime: 2023-10-18 15:19:33
+ * @LastEditTime: 2023-10-19 12:23:03
  * @FilePath: \ThingsPanel-Backend-Vue\src\view\pages\console\Setting.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -14,26 +14,25 @@
             <el-form class="console-setting-form el-dark-input" label-position="left" label-width="120px"
                 ref="settingFormRef" :model="formData" :rules="formRules">
 
-            <el-form-item label="看板编号">
+            <el-form-item label="看板编号" prop="template_code">
                 {{ data.code }}
             </el-form-item>
 
-            <el-form-item label="看板名称">
+            <el-form-item label="看板名称" prop="name">
                 <el-input v-model="formData.name"></el-input>
             </el-form-item>
 
             <el-form-item label="看板背景颜色">
-                <el-input v-model="formData.console_name"></el-input>
-
+                <el-color-picker :disabled="true" v-model="formData.configData.background"></el-color-picker>
             </el-form-item>
             <el-row>
-                <el-checkbox v-model="formData.dragable">组件是否可拖拽</el-checkbox>
+                <el-checkbox :disabled="true" v-model="formData.configData.dragable">组件是否可拖拽</el-checkbox>
             </el-row>
             <el-row>
-                <el-checkbox v-model="formData.showTools">是否显示组件工具栏</el-checkbox>
+                <el-checkbox :disabled="true" v-model="formData.configData.showTools">是否显示组件工具栏</el-checkbox>
             </el-row>
             <el-row>
-                <el-checkbox v-model="formData.editTitle">是否可编辑组件标题</el-checkbox>
+                <el-checkbox :disabled="true" v-model="formData.configData.editTitle">是否可编辑组件标题</el-checkbox>
             </el-row>
 
             </el-form>
@@ -46,6 +45,8 @@
 </template>
 
 <script>
+import ConsoleAPI from "@/api/console.js";
+import { message_success } from "@/utils/helpers.js";
 export default {
     components: {},
     props: {
@@ -62,21 +63,70 @@ export default {
         dialogVisible: {
             get() { return this.visible },
             set(val) { this.$emit("update:visible", val) }
-        }
+        },
+        // // 表单数据
+        // formData: {
+        //     get() {
+        //         let { id, name, code: template_code, config } = this.data;
+        //         let configData = {
+        //             background: "",
+        //             dragable: false,
+        //             showTools: false,
+        //             editTitle: false
+        //         }
+        //         return { id, name, template_code, configData };
+        //     }
+        // }
     },
     data() {
         return {
-            // 表单数据
             formData: {
-                template_code: "123456"
+                name: "",
+                code: "",
+                config: {
+                    background: "",
+                    dragable: false,
+                    showTools: false,
+                    editTitle: false
+                }
             },
             // 表单验证
             formRules: {},
         }
     },
+    watch: {
+        data: {
+            handler(newValue) {
+                if (newValue) {
+                    let { id, name, code, config } = this.data;
+                    this.formData = { 
+                        id, 
+                        name, 
+                        code, 
+                        configData: {
+                            background: "",
+                            dragable: false,
+                            showTools: false,
+                            editTitle: false
+                        }
+                    };
+                }
+            }
+        }
+    },
     methods: {
         handleSubmit() {
-
+            console.log("setting", this.formData);
+            // this.formData.config = JSON.stringify(this.formData.configData);
+            delete this.formData.code;
+            ConsoleAPI.edit(this.formData)
+                .then(({ data: result }) => {
+                    console.log(result);
+                    if (result.code === 200) {
+                        message_success("保存成功");
+                        this.dialogVisible = false;
+                    }
+                })
         }
     }
 }
