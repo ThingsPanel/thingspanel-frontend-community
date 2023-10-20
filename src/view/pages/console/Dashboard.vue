a26550f1191598cf72260c3b39290a53a010714a
chaoxiaoshu-mx <leukotrichia@163.com>
2023-10-20 10:06:53 +0800

    fix: 解决看板添加组件时的布局问题

--- src/view/pages/console/Dashboard.vue
+++ src/view/pages/console/Dashboard.vue
@@ -2,7 +2,7 @@
  * @Author: chaoxiaoshu-mx leukotrichia@163.com
  * @Date: 2023-10-12 20:49:12
  * @LastEditors: chaoxiaoshu-mx leukotrichia@163.com
- * @LastEditTime: 2023-10-20 09:08:53
+ * @LastEditTime: 2023-10-20 10:04:45
  * @FilePath: \ThingsPanel-Backend-Vue\src\view\pages\console\Dashboard.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -276,6 +276,15 @@ export default {
         this.editData.data.push({ uId: item.uId, deviceId })
       })
       this.editData.template = this.getDefaultLayout(opts, 4);
+      this.$nextTick(() => {
+        this.editData.template.forEach(item => {
+        let ref = this.$refs["component_" + item.i];
+        if (ref && ref[0]) {
+          ref[0].sizeChange();
+        }
+      })
+      })
+      
     },
     /**
      * @description: 全屏
