492ef29550998aa60f89306a6936a2bc7ebdba9b
songren <732054167@qq.com>
2024-06-07 17:33:31 +0800

    feat(projects): update bug

--- src/views/device/details/modules/command-delivery.vue
+++ src/views/device/details/modules/command-delivery.vue
@@ -9,6 +9,7 @@ defineProps<{
 
 // 状态
 const formatStatus = status => {
+  console.log(status);
   switch (status) {
     case '1':
       return $t('generate.sendingSuccess');
@@ -24,17 +25,34 @@ const formatStatus = status => {
 };
 
 const columns = [
-  { title: $t('device_template.table_header.commandIdentifier'), minWidth: '140px', key: 'identify' },
-  { title: $t('device_template.table_header.commandName'), minWidth: '140px', key: '' },
+  {
+    title: $t('device_template.table_header.commandIdentifier'),
+    minWidth: '140px',
+    key: 'identify'
+  },
+  {
+    title: $t('device_template.table_header.commandName'),
+    minWidth: '140px',
+    key: ''
+  },
   {
     title: $t('generate.commandIssuanceTime'),
     minWidth: '140px',
     key: 'created_at',
     render: row => dayjs(row.created_at).format('YYYY-MM-DD HH:mm:ss')
   },
-  { title: $t('generate.status'), minWidth: '140px', key: 'status', render: row => formatStatus(row.status) },
+  {
+    title: $t('generate.status'),
+    minWidth: '140px',
+    key: 'status',
+    render: row => formatStatus(row.status)
+  },
   { title: $t('generate.commandConetnt'), minWidth: '140px', key: 'data' },
-  { title: $t('generate.errorMessage'), minWidth: '140px', key: 'error_message' }
+  {
+    title: $t('generate.errorMessage'),
+    minWidth: '140px',
+    key: 'error_message'
+  }
 ];
 </script>
 
