0744023322df98b5e6bf64dcaeda7e5735d9c380
songren <732054167@qq.com>
2024-06-07 15:13:00 +0800

    feat(projects): update code view

--- src/views/device/details/modules/settings.vue
+++ src/views/device/details/modules/settings.vue
@@ -173,7 +173,7 @@ const selectConfig = v => {
   deviceUpdateConfig({ device_id: props.id, device_config_id: v });
   deviceDataStore.fetchData(props.id);
   initData();
-  console.log('成功啦,77777777777');
+  console.log('成功啦,999999999999');
   emit('change');
 };
 </script>
