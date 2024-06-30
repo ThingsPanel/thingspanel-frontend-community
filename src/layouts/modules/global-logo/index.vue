189788a2907dca1dd51694a8f512287c5fd2eecc
zhanglei <916256628@qq.com>
2024-06-30 23:15:56 +0800

    fix: [The system configuration of the Community Edition does not take effect when changing the system title]

--- src/layouts/modules/global-logo/index.vue
+++ src/layouts/modules/global-logo/index.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { $t } from '@/locales';
+import { useSysSettingStore } from '@/store/modules/sys-setting';
 
 defineOptions({
   name: 'GlobalLogo'
 });
+const sysSettingStore = useSysSettingStore();
 
 interface Props {
   /** Whether to show the title */
@@ -19,7 +21,7 @@ withDefaults(defineProps<Props>(), {
   <RouterLink to="/" class="w-full flex-center nowrap-hidden">
     <SystemLogo class="text-64px text-primary" />
     <h2 v-show="showTitle" class="pl-8px text-16px text-primary font-bold transition duration-300 ease-in-out">
-      {{ $t('title') }}
+      {{ sysSettingStore.system_name === '' ? $t('title') : sysSettingStore.system_name }}
     </h2>
   </RouterLink>
 </template>
