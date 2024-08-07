1e99b02b434162286e80bd13a1bf9353c73d04a0
zhanglei <916256628@qq.com>
2024-08-07 09:59:56 +0800

    fix: [Device Details - Add Confirmation Pop up]

--- src/views/device/details/modules/telemetry/telemetry.vue
+++ src/views/device/details/modules/telemetry/telemetry.vue
@@ -262,8 +262,7 @@ const copy = event => {
   document.execCommand('copy');
   window.$message?.success($t('theme.configOperation.copySuccess'));
 };
-
-const sends = async () => {
+const handlePositiveClick = async () => {
   if (isJSON(formValue.value)) {
     // 发送属性的逻辑...
     const { error } = await telemetryDataPub({
@@ -277,6 +276,7 @@ const sends = async () => {
     }
   }
 };
+
 const onTapTableTools = (i: any) => {
   if (typeof i.value === 'number') {
     modelType.value = $t('custom.device_details.sequential');
@@ -490,7 +490,13 @@ const inputFeedback = computed(() => {
           </n-form-item>
           <n-space align="end">
             <n-button @click="showDialog = false">{{ $t('generate.cancel') }}</n-button>
-            <n-button type="primary" @click="sends">{{ $t('generate.send') }}</n-button>
+
+            <n-popconfirm @positive-click="handlePositiveClick">
+              <template #trigger>
+                <n-button type="primary">{{ $t('generate.send') }}</n-button>
+              </template>
+              确定发送指令吗
+            </n-popconfirm>
           </n-space>
         </n-form>
       </n-card>
