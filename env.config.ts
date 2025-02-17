59f16328e18da170df50a2d4be2a4abac86e9b76
zhanglei <10709028+zhanglei987654321@user.noreply.gitee.com>
2025-02-17 15:44:07 +0800

    fix: [增加api key]

--- env.config.ts
+++ env.config.ts
@@ -7,8 +7,8 @@ export function createServiceConfig(env: Env.ImportMeta) {
   const mockURL = 'https://mock.apifox.com/m1/4080832-0-default';
   // const devURL = 'http://47.92.253.145:9999/api/v1';
   // const devURL = '  http://47.115.210.16:8080/api/v1';
-  const devURL = 'http://c.thingspanel.cn/api/v1';
-  // const devURL = 'http://demo.thingspanel.cn/api/v1';
+  // const devURL = 'http://c.thingspanel.cn/api/v1';
+  const devURL = 'http://demo.thingspanel.cn/api/v1';
 
   const testURL = '';
   const prodURL = '';
