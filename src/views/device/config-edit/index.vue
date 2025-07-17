c1921162ce24de6ba56073b032ffe941ddc5b627
Wrong <1070750088@qq.com>
2025-07-17 23:34:14 +0800

    style(device/config-edit): 修复表单缩进不一致问题

--- src/views/device/config-edit/index.vue
+++ src/views/device/config-edit/index.vue
@@ -383,7 +383,7 @@ function handleDeviceTypeChange(newValue: string | number) {
             ></NSelect>
           </NFormItem>
         </template>
-      <NFormItem v-if="configForm.device_type && formElements.length > 0">
+        <NFormItem v-if="configForm.device_type && formElements.length > 0">
           <FormInput v-model:protocol-config="protocol_config" :form-elements="formElements"></FormInput>
         </NFormItem>
         <NFlex justify="flex-start">
