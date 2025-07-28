f069c84f602e943e3a7c1425c8917834bdf0f6ad
Wrong <1070750088@qq.com>
2025-07-28 17:44:59 +0800

    feat(global-footer): 使用系统设置中的名称替换固定版权文本
    从系统设置存储中获取系统名称并动态显示在页脚版权信息中，提高灵活性

--- src/layouts/modules/global-footer/index.vue
+++ src/layouts/modules/global-footer/index.vue
@@ -2,12 +2,13 @@
 defineOptions({
   name: 'GlobalFooter'
 })
+const sysSettingStore = useSysSettingStore()
 </script>
 
 <template>
   <DarkModeContainer class="h-full flex-center">
     <a href="https://github.com/ThingsPanel/thingspanel-ui/blob/main/LICENSE" target="_blank" rel="noopener noreferrer">
-      Copyright © 2024 ThingsPanel
+      Copyright © 2024 {{ sysSettingStore.system_name }}
     </a>
   </DarkModeContainer>
 </template>
