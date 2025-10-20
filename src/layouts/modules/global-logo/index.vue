689cb06231bec10e18a40749daaaaa5e06f3d3ea
zhaojie <mr_zhaojie@163.com>
2025-10-20 17:05:37 +0800

    refactor(logo): 更新系统logo组件和全局logo布局
    - 修改系统logo组件，动态设置高度以适应宽度
    - 重构全局logo布局，添加logo容器以优化样式和对齐方式

--- src/layouts/modules/global-logo/index.vue
+++ src/layouts/modules/global-logo/index.vue
@@ -19,13 +19,20 @@ withDefaults(defineProps<Props>(), {
 
 <template>
   <RouterLink to="/" class="w-full flex items-center nowrap-hidden ml-3">
-    <SystemLogo width="32" />
-    <div v-show="showTitle" class="pl-8px mt-2">
-      <n-ellipsis style="max-width: 150px" class="text-16px text-primary font-bold transition duration-300 ease-in-out">
-        {{ sysSettingStore.system_name === '' ? $t('title') : sysSettingStore.system_name }}
-      </n-ellipsis>
+    <div class="logo-box">
+      <SystemLogo width="32" />
+      <div v-show="showTitle" class="pl-8px">
+        <n-ellipsis style="max-width: 150px" class="text-16px text-primary font-bold transition duration-300 ease-in-out">
+          {{ sysSettingStore.system_name === '' ? $t('title') : sysSettingStore.system_name }}
+        </n-ellipsis>
+      </div>
     </div>
   </RouterLink>
 </template>
 
-<style scoped></style>
+<style scoped>
+  .logo-box {
+    display: flex;
+    align-items: center;
+  }
+</style>
